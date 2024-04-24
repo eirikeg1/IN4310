47764cc9f1f57469cfa76d7aa27532d5dca328f6
eirikeg1 <eirikeg@ifi.uio.no>
2024-04-24 15:26:12 +0200

    implement u-net class

--- mandatory2/resnet_unet.py
+++ mandatory2/resnet_unet.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 def double_conv(in_channels, out_channels):
@@ -58,17 +59,26 @@ class Decoder(nn.Module):
     def __init__(self, out_channels=1):
         super().__init__()
         # TODO: Initialise the layers with correct number of input and output channels
-        self.up_conv6 = None  # use the up_conv() function. Number of output channels = 512
-        self.conv6 = None  # use the double_conv() function. Number of output channels = 512
-        self.up_conv7 = None  # use the up_conv() function. Number of output channels = 256
-        self.conv7 = None  # use the double_conv() function. Number of output channels = 256
-        self.up_conv8 = None  # use the up_conv() function. Number of output channels = 128
-        self.conv8 = None  # use the double_conv() function. Number of output channels = 128
-        self.up_conv9 = None  # use the up_conv() function. Number of output channels = 64
-        self.conv9 = None  # use the double_conv() function. Number of output channels = 64
-        self.up_conv10 = None  # use the up_conv() function. Number of output channels = 32
-        self.conv10 = double_conv(32, out_channels=out_channels)  # Use nn.Conv2d with kernel size 1 to get the segmentation mask
-
+        
+        """
+        Block1: 64
+        Block2: 128
+        Block3: 256
+        Block4: 512
+        Block5: 512
+        """
+        
+        self.up_conv6 = up_conv(2*512, 512)  # use the up_conv() function. Number of output channels = 512
+        self.conv6 = double_conv(1024, 512)  # use the double_conv() function. Number of output channels = 512
+        self.up_conv7 = up_conv(512, 256)  # use the up_conv() function. Number of output channels = 256
+        self.conv7 = double_conv(512, 256)  # use the double_conv() function. Number of output channels = 256
+        self.up_conv8 = up_conv(256, 128)  # use the up_conv() function. Number of output channels = 128
+        self.conv8 = double_conv(256, 128)  # use the double_conv() function. Number of output channels = 128
+        self.up_conv9 = up_conv(128, 64)  # use the up_conv() function. Number of output channels = 64
+        self.conv9 = double_conv(192, 64)  # use the double_conv() function. Number of output channels = 64
+        self.up_conv10 = up_conv(64, 32)  # use the up_conv() function. Number of output channels = 32
+        self.conv10 = self.conv10 = nn.Conv2d(32, out_channels=out_channels, kernel_size=1)  # Use nn.Conv2d with kernel size 1 to get the segmentation mask
+        
         self._weights_init()
 
     def _weights_init(self):
@@ -78,16 +88,45 @@ class Decoder(nn.Module):
             elif isinstance(m, nn.BatchNorm2d):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
+                
 
     def forward(self, encoder1_blocks, encoder2_blocks):
         # TODO: Implement the forward which concatenates the outputs of the two encoders
         #       at each stage/block to use as the input
+        
+        a1, a2, a3, a4, a5 = encoder1_blocks
+        b1, b2, b3, b4, b5 = encoder2_blocks
+        
+        concat = torch.cat((a5, b5), dim=1)
+        
+        prev_conv = self.up_conv6(concat)
+        concat = torch.cat((prev_conv, a4, b4), dim=1)
+        prev_conv = self.conv6(concat)
+        
+        prev_conv = self.up_conv7(prev_conv)
+        concat = torch.cat((prev_conv, a3, b3), dim=1)
+        prev_conv = self.conv7(concat)
+        
+        prev_conv = self.up_conv8(prev_conv)
+        concat = torch.cat((prev_conv, a2, b2), dim=1)
+        prev_conv = self.conv8(concat)
+        
+        prev_conv = self.up_conv9(prev_conv)
+        concat = torch.cat((prev_conv, a1, b1), dim=1)
+        prev_conv = self.conv9(concat)
+        
+        prev_conv = self.up_conv10(prev_conv)
+        output = self.conv10(prev_conv)
+        
+        
         # TODO: Replace the 1st "1" below in torch.Size with your batch size
-        assert output.shape == torch.Size([1, 1, 224, 224]), \
+        assert output.shape == torch.Size([64, 1, 224, 224]), \
             f"The output shape should be same as the input image's shape but it is {output.shape} instead."
 
+        return output
 
 class TwoEncodersOneDecoder(nn.Module):
+    
     def __init__(self, encoder, pretrained=True, out_channels=1):
         """
         The segmentation model to be used.
@@ -98,10 +137,14 @@ class TwoEncodersOneDecoder(nn.Module):
         """
         super().__init__()
         # TODO: Initialise the encoders and the decoder
-        self.encoder1 = None
-        self.encoder2 = None
-        self.decoder = None
+        self.encoder1 = Encoder(encoder, pretrained=pretrained)
+        self.encoder2 = Encoder(encoder, pretrained=pretrained)
+        self.decoder = Decoder(out_channels=out_channels)
 
     def forward(self, x, h_x):
         # TODO: Implement the forward pass calling the encoders and passing the outputs to the decoder
-        pass
+        assert x.shape == h_x.shape,f"Input and H-stain image should have the same shape but they have {x.shape} and {h_x.shape} respectively."
+        encoder1_blocks = self.encoder1(x)
+        encoder2_blocks = self.encoder2(h_x)
+        out = self.decoder(encoder1_blocks, encoder2_blocks)
+        return out
